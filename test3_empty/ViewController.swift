bd96c86c1b967c4daada0f01d9f1ddead7da9c0c
Sławomir Kurczewski <slawomir.kurczewski@gmail.com>
2020-02-14 09:21:50 +0100

    Initial Commit

---
+++ test3_empty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test3_empty
+//
+//  Created by Slawek Kurczewski on 14/02/2020.
+//  Copyright © 2020 Slawomir Kurczewski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ test3_emptyTests/test3_emptyTests.swift
@@ -0,0 +1,34 @@
+//
+//  test3_emptyTests.swift
+//  test3_emptyTests
+//
+//  Created by Slawek Kurczewski on 14/02/2020.
+//  Copyright © 2020 Slawomir Kurczewski. All rights reserved.
+//
+
+import XCTest
+@testable import test3_empty
+
+class test3_emptyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
