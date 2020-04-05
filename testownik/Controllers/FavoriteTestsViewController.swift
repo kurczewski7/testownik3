e9c1895b10eae622838bc19dc52435e9f7b2d2ed
Sławomir Kurczewski <slawomir.kurczewski@gmail.com>
2020-04-05 19:48:52 +0200

    FavoriteTests edited. OK

--- testownik/Controllers/FavoriteTestsViewController.swift
+++ testownik/Controllers/FavoriteTestsViewController.swift
@@ -9,15 +9,44 @@
 import UIKit
 
 class FavoriteTestsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    let docum = [[1,2,3,4],[5,6,7,8,9,10]]
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return docum.count
+    }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       return 5
+        return docum[section].count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        cell.textLabel?.text = "AAA:\(indexPath.row)"
-        cell.detailTextLabel?.text = "bbb"
+        let cell = tableView.dequeueReusableCell(withIdentifier: "favoriteCell") as! FavoriteTestsTableViewCell
+        cell.label1.text = "AAAA: \(docum[indexPath.section][indexPath.row])"
+        cell.label2.text = "aaaa: \(indexPath.section)"
+        cell.label3.text = "cccc: \(indexPath.row)"
         return cell
+    }
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let label = UILabel()
+        label.text = "AAAA: \(section)"
+        label.textColor = .red
+        return label
+    }
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let action = UIContextualAction(style: .normal, title: "AAAA") { (act, view, exec) in
+            print("trailingSwipeActionsConfigurationForRowAt")
+        }
+        action.backgroundColor = .green
+        let swipe = UISwipeActionsConfiguration(actions: [action])
+        return swipe
+    }
+    func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+               let action = UIContextualAction(style: .normal, title: "BBBB") { (act, view, exec) in
+            print("leadingSwipeActionsConfigurationForRowAt")
+        }
+        action.backgroundColor = .red
+        let swipe = UISwipeActionsConfiguration(actions: [action])
+        return swipe
+
     }
     
 
