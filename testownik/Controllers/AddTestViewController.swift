831d60b628c9b245d7a6f6c948497b5ab47a3b78
Sławomir Kurczewski <slawomir.kurczewski@gmail.com>
2020-04-13 19:27:06 +0200

    Unwidd OL

--- testownik/Controllers/AddTestViewController.swift
+++ testownik/Controllers/AddTestViewController.swift
@@ -18,56 +18,23 @@ class AddTestViewController: UIViewController {
     @IBOutlet weak var label: UILabel!
 
     
-
-    @IBAction func cancelNavigatorButton(_ sender: UIBarButtonItem) {
-         self.dismiss(animated: true, completion: nil)
+    @IBAction func saveButtonPressed(_ sender: UIButton) {
+        print("saveButtonPressed")
+        self.performSegue(withIdentifier: "goToUnwindCloudVC", sender: nil)
+    }
+    @IBAction func cancelButtonPressed(_ sender: Any) {
+        print("cancelButtonPressed")
     }
     
-    @IBAction func buttonPress(_ sender: UISegmentedControl) {
-        //sender.selectedSegmentIndex = -1
-        if sender.selectedSegmentIndex == 0 {
-            sender.selectedSegmentTintColor = .red
-            //segment.selectedSegmentTintColor = .red
-             print("Cancel pressed")
-            //self.dismiss(animated: true)
-            //self.dismiss(animated: true, completion: nil)
-        }
-         if sender.selectedSegmentIndex == 1 {
-            sender.selectedSegmentTintColor = .green
-           print("Save pressed")
-        }
+    @IBAction func cancelNavigatorButton(_ sender: UIBarButtonItem) {
+         //self.dismiss(animated: true, completion: nil)
+        print("cancelNavigatorButton")
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-//        segment.selectedSegmentIndex = -1
-//        //segment.setTitle("Anuluj", forSegmentAt: 0)
-//        //segment.setTitle("Zapisz", forSegmentAt: 1)
-//        let imageOk = UIImage(named: "save_filled")?.withRenderingMode(UIImage.RenderingMode.alwaysTemplate)
-//        let imageCancel = UIImage(named: "cancel_filled")?.withTintColor(.systemRed)
-//        imageCancel?.withTintColor(.cyan)
-//
-//
-//        if #available(iOS 13.0, *) {
-//           segment.setTitleTextAttributes([.foregroundColor: UIColor.white], for: .selected)
-//           segment.selectedSegmentTintColor = UIColor.blue
-//        } else {
-//           segment.tintColor = UIColor.blue
-//        }
-//
-//
-//        segment.setImage(imageCancel, forSegmentAt: 0)
-//        segment.setImage(imageOk, forSegmentAt: 1)
-//        segment.tintColor = .purple
-        //segment.selectedSegmentTintColor = .red
+
         label.text =  getCurrentDate()
-      
-        
-//        let origImage = UIImage(named: "imageName")
-//        let tintedImage = origImage?.withRenderingMode(.alwaysTemplate)
-//        btn.setImage(tintedImage, for: .normal)
-//        btn.tintColor = .red
-        
-        
         if documentsValue.count > 0 {
             textField2.text = "\(documentsValue[0].myTexts)"
             textField3.text = "\(documentsValue.count)"
